96f19f7637afce7bf4ccc81fbdb3b5a7a55fcafd
Alex Mancini <alex_mancini@sfu.ca>
2025-09-30 19:38:47 -0700

    Final Code

--- main.js
+++ main.js
@@ -1,65 +1,7 @@
-console.log("script.js is loaded");
-
-
-
+console.log("main.js is loaded");
 
 let mainTitle = document.querySelector("#main-title");
 
 mainTitle.addEventListener("click", function () { 
-    alert("You clicked the main title!");
+    alert("Why are you clicking this buddy?");
  });
-
-
-/*var c = document.getElementById("chart");
-var ctx = c.getContext("2d");
-
-ctx.beginPath();
-ctx.fillRect(100,100, 300, 75);
-ctx.fillRect(100,200, 100, 75);
-ctx.fillRect(100,300, 200, 75);
-ctx.closePath();*/
-
-
-/*let workouts=[
-"Cardio",
-"Push day",
-"Pull day",
-"Leg day",
-];
-console.log(workouts);
-
-let times=[
-3,
-1,
-2,
-0,
-];
-console.log(times);
-
-
-const data = workouts.map((element, index) => {
-    [element, times[index]];
-});
-
-//let data= workouts.map((workout)=>({workout, times}));
-
-console.log(data);
-
-const chart = document.querySelector(".chart");
-
-const createBarItems = (workoutItem) => {
-let item = document.createElement("div");
-let bar = document.createElement("span");
-let title = document.createElement("span");
-
-bar.style = `workout:${workoutItem}`;
-title.innerText=workoutItem.workout;
-
-bar.append(title);
-item.append(bar);
-
-return item;
-};
-
-data.forEach((workout) => chart.append(createBarItems(workout)));*/
-
--- vis.js
+++ vis.js
@@ -6,7 +6,6 @@ let triangle = document.querySelector("polygon");
 
 
     triangle.addEventListener("click", function() {
-        //alert("You clicked the triangle");
         let randomColor = Math.floor(Math.random() * 1677721);
         triangle.setAttribute("fill", `#${randomColor}`);
     });
